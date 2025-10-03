eb201aee7faed1728db97436623cb579059c4a0d
WizMager <wizmager@gmailcom>
2025-10-03 23:02:26 +0300

    -add logic of instantiate and reuse craft items

--- Assets/Scripts/Ui/Realization/InteractObjectStatusWindow/CraftItem.cs
+++ Assets/Scripts/Ui/Realization/InteractObjectStatusWindow/CraftItem.cs
@@ -23,12 +23,31 @@ namespace Ui.Realization.InteractObjectStatusWindow
             _craftItemImage.sprite = sprite;
             _itemType = itemType;
             _craftItemName.text = itemType.ToString();
-            transform.SetParent(parent);
+            ParentingTo(parent);
             transform.localScale = Vector3.one; //TODO: crutch, scale change when add GO in scroll view
             
             _craftItemImage.OnPointerClickAsObservable().Subscribe(_ => OnChoose()).AddTo(this);
         }
 
+        public void Activate(RectTransform parent)
+        {
+            _craftItemImage.enabled = true;
+            _craftItemName.enabled = true;
+            ParentingTo(parent);
+        }
+        
+        public void Deactivate(Transform parent)
+        {
+            transform.SetParent(parent);
+            _craftItemImage.enabled = false;
+            _craftItemName.enabled = false;
+        }
+        
+        private void ParentingTo(RectTransform parent = null)
+        {
+            transform.SetParent(parent);
+        }
+        
         private void OnChoose()
         {
             _itemChooseCommand?.Execute(_itemType);
--- Assets/Scripts/Ui/Realization/InteractObjectStatusWindow/InteractObjectStatusController.cs
+++ Assets/Scripts/Ui/Realization/InteractObjectStatusWindow/InteractObjectStatusController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Data.ItemsData;
 using Game.Services.InteractObjectService;
-using Game.Services.ItemStorageService;
 using Game.Services.TimerService;
 using R3;
 using Ui.UiCore;
@@ -20,7 +19,8 @@ namespace Ui.Realization.InteractObjectStatusWindow
         private readonly IItemData _itemData;
         private readonly CompositeDisposable _disposable = new ();
         private readonly Dictionary<EInteractObject, CraftItem[]> _craftItems = new ();
-        
+        private readonly Transform _craftItemsContainer;
+
         private float _currentTimeForAction;
         
         public InteractObjectStatusController(
@@ -34,6 +34,7 @@ namespace Ui.Realization.InteractObjectStatusWindow
             _timerService = timerService;
             _itemData = itemData;
 
+            _craftItemsContainer = new GameObject("CraftItemsContainer").transform;
             View.InteractButton.OnClickAsObservable().Subscribe(_ => OnInteractButtonClicked().Forget()).AddTo(_disposable);
         }
 
@@ -55,6 +56,13 @@ namespace Ui.Realization.InteractObjectStatusWindow
                 
                 _craftItems.Add(interactObject, craftItems.ToArray());
             }
+            else
+            {
+                foreach (var craftItem in _craftItems[interactObject])
+                {
+                    craftItem.Activate(View.ScrollViewContentTransform);
+                }
+            }
             
             View.ObjectName.text = _interactObjectService.GetCurrentInteractObjectData().InteractObjectName.ToString();
             _currentTimeForAction = _interactObjectService.GetCurrentInteractObjectData().BaseTimeForAction;
@@ -64,7 +72,20 @@ namespace Ui.Realization.InteractObjectStatusWindow
             
             ChangeLastActionTimerValue(TimeSpan.FromSeconds(_currentTimeForAction));
         }
-        
+
+        protected override void OnHide()
+        {
+            if (_craftItems.Count == 0)
+                return;
+            
+            var interactObject = _interactObjectService.GetCurrentInteractObjectData().InteractObjectName;
+            
+            foreach (var craftItem in _craftItems[interactObject])
+            {
+                craftItem.Deactivate(_craftItemsContainer);
+            }
+        }
+
         private async UniTask OnInteractButtonClicked()
         {
             while (true)
